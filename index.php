cd84f969451729b1da34ab561542396dd71f2320
josh <joshuaholman5@gmail.com>
2024-11-07 21:09:24 -0800

    added basic css

--- index.php
+++ index.php
@@ -1,26 +1,35 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="styles.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
   <title>DB Access Page</title>
 </head>
 
 <body>
-<h1>University database portal</h1>
 
-<form action="professor.php" method="post">
-  <label for="prof_ssn">Professor ssn lookup:</label>
-  <input name="prof_ssn" id="prof_ssn" type="text">
-  <button type="submit">Submit</button>
-</form>
+  <h1 class="center">University database portal</h1>
 
-<br>
+  <div class="center">
 
-<form action="student.php" method="post">
-  <label for="course_id">Course id lookup:</label>
-  <input name="course_id" id="course_id" type="text">
-  <button type="submit">Submit</button>
-</form>
+    <form action="professor.php" method="post">
+      <label for="prof_ssn">Professor ssn lookup:</label>
+      <input name="prof_ssn" id="prof_ssn" type="text">
+      <button type="submit">Submit</button>
+    </form>
+
+    <br>
+
+    <form action="student.php" method="post">
+      <label for="course_id">Course id lookup:</label>
+      <input name="course_id" id="course_id" type="text">
+      <button type="submit">Submit</button>
+    </form>
+  </div>
 </body>
 
 </html>
