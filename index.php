d6c523a987311c7341967049c61c1495f7c4eed3
josh <joshuaholman5@gmail.com>
2024-12-14 17:32:22 -0800

    spaghettified css

--- index.php
+++ index.php
@@ -5,16 +5,22 @@ include 'include/header.php';
 
 <div class="container">
   <h1>Welcome! Choose a Page:</h1>
-  <div>
-    <h2>For the Professors</h2>
-    <a href="src/professor_details_pquery.php" class="button">Professor Details</a>
-    <a href="src/grade_counts_pquery.php" class="button">Grade Counts</a>
+  <div class="buttons-container">
+    <div class="button-group">
+      <h2>For the Professors</h2>
+      <br>
+      <a href="src/professor_details_pquery.php" class="button">Professor Details</a>
+      <a href="src/grade_counts_pquery.php" class="button">Grade Counts</a>
+    </div>
+
+    <div class="button-group">
+      <h2>For the Students</h2>
+      <br>
+      <a href="src/section_listings_squery.php" class="button student">Section Listings</a>
+      <a href="src/student_schedule_squery.php" class="button student">Student Schedule</a>
+    </div>
   </div>
-  <div>
-    <h2>For the Students</h2>
-    <a href="src/section_listings_squery.php" class="button">Section Listings</a>
-    <a href="src/student_schedule_squery.php" class="button">Student Schedule</a>
-  </div>
+</div>
 </div>
 
 <?php include 'include/footer.php'; ?>
