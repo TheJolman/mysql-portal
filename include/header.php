7181a600b961081bf40518c01e549bfc7e74db75
josh <joshuaholman5@gmail.com>
2024-12-14 16:58:04 -0800

    using dynamic path for stylesheet location

--- include/header.php
+++ include/header.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale-1.0">
-    <link rel="icon" type="image/x-icon" href="../public/favicon.ico">
-    <link rel="stylesheet" href="../public/main.css">
-    <title><?php echo $pageTitle ?? 'Portal'; ?></title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale-1.0">
+  <link rel="icon" type="image/x-icon" href="../public/favicon.ico">
+  <link rel="stylesheet" href="<?php echo rtrim(dirname($_SERVER['PHP_SELF']), '/src'); ?>/public/main.css">
+  <title><?php echo $pageTitle ?? 'Portal'; ?></title>
 </head>
+
 <body>
