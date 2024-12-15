d955743dddfb5bb693c9cd18e1e1a5cd1d54e656
josh <joshuaholman5@gmail.com>
2024-12-14 18:40:04 -0800

    added simple nav bar and simplified some hrefs

--- include/header.php
+++ include/header.php
@@ -4,9 +4,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale-1.0">
-  <link rel="icon" type="image/x-icon" href="../public/favicon.ico">
-  <link rel="stylesheet" href="<?php echo rtrim(dirname($_SERVER['PHP_SELF']), '/src'); ?>/public/main.css">
+  <link rel="icon" type="image/x-icon" href="/public/favicon.ico">
+  <link rel="stylesheet" href="/public/main.css">
   <title><?php echo $pageTitle ?? 'Portal'; ?></title>
 </head>
 
 <body>
+    <nav class="banner">
+      <a href="/index.php" class="home-button">Home</a>
+    </nav>
