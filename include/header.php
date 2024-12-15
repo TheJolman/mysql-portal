8734d7e9a5b2b7d536600aa114b0fbf308a32f2d
josh <joshuaholman5@gmail.com>
2024-12-14 18:41:25 -0800

    nvm it needs to be like that

--- include/header.php
+++ include/header.php
@@ -4,8 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale-1.0">
-  <link rel="icon" type="image/x-icon" href="/public/favicon.ico">
-  <link rel="stylesheet" href="/public/main.css">
+  <link rel="icon" type="image/x-icon" href="<?php echo rtrim(dirname($_SERVER['PHP_SELF']), '/src'); ?>/public/favicon.ico">
+  <link rel="stylesheet" href="<?php echo rtrim(dirname($_SERVER['PHP_SELF']), '/src'); ?>/public/main.css">
   <title><?php echo $pageTitle ?? 'Portal'; ?></title>
 </head>
 
